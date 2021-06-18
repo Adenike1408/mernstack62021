521651a9d4adc6d07cc64fc8b8aca319b6c81e11
Ashish <ash.ishu025g@gmail.com>
2021-06-18 22:57:01 +0530

    constructor function

---
+++ CoreJS/classUsingFunctions.js
@@ -0,0 +1,16 @@
+//How to create classes using function
+
+var userClass = function(name, age, address){ //constructor function
+    this.name = name;
+    this.age = age;
+    this.address = address;
+    this.getAddress = function(){
+        return this.address;
+    }
+}
+
+var userObj = new userClass("Luke", 21, "Main Street NY") //instantiating the class object
+
+console.log(userObj.age);
+
+console.log(userObj.getAddress())
