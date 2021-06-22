f0b5d4ff3c4d64186c05be15506f23fa584eafc9
Ashish <ash.ishu025g@gmail.com>
2021-06-22 21:02:37 +0530

    iife

--- CoreJS/functions.js
+++ CoreJS/functions.js
@@ -1,16 +1,32 @@
 //functions
 
-console.log(myfunc())
+//console.log(myfunc())
 
-var name = "Adenike";
-printName(name) //function invocation by opening and closing a paranthesis
+// var name = "Adenike";
+// printName(name) //function invocation by opening and closing a paranthesis
 
-//named function
-function printName(params) {
-    console.log("Priting Name ", params)
-}
+// //named function
+// function printName(params) {
+//     console.log("Priting Name ", params)
+// }
 
-//function expression
-var myfunc = function (params) {
-    console.log("Priting Myfunc ", params)
-}
+// //function expression
+// var myfunc = function (params) {
+//     console.log("Priting Myfunc ", params)
+// }
+
+
+//IIFE - Immediately Invocable Function Expression
+// (function(){ // this will only be a single instance
+//     console.log("We all are hopeful for a better future!!!")
+//     global.user = {name:"Test"}
+// })()
+//console.log(global.user)
+
+
+(function (params) {
+    console.log(params)
+    global.user = {name:"Adenike"}
+})("Hung");
+
+console.log(global.user)
