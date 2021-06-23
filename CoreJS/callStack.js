9fd8ca4b5bebada3852c2c39289a96d1cf566d7a
Ashish <ash.ishu025g@gmail.com>
2021-06-23 23:18:48 +0530

    callstack

---
+++ CoreJS/callStack.js
@@ -0,0 +1,39 @@
+//Execution in javascipt uses Stack like other languages and this format is LIFO : Last In First Out
+//call stack : LIFO - last in first out
+//call stack visualization upto and extent
+
+function foo(){
+    throw  new Error("Foo is not a good function");
+}
+
+
+function bar(){
+    foo();
+}
+
+function baz(){
+    bar();
+}
+
+baz();
+
+console.log("Execution Started")
+
+
+//blowing up the stack 
+
+function ExecutionExceeds(){
+    ExecutionExceeds()
+}
+
+ExecutionExceeds()
+
+// Practice Questions - 
+//=> create an example of print function which contains a callback function and prints user details, where user is an objet with name, age, address, exepertise
+//=> the same callback function should be used to print account details - account name, number, balance, account type, branch
+//=> create a class(Through Object or Constructor Function) Name vaccine which contains various properties like name, brand, efficay, etc 
+//=> and functions in vaccine like howToTake() and preservationDetails(), upon execution it should give us desired info like how many doses and what temperature to use
+//=> give me an example of callStack - and blow up the stack
+//=> how functions are special case in javascript, try giving an example by creating a class using functions - Parent should be Person with some details 
+//   and Inherited function should be Employee that will have EmpID, EmpName, Designaiton, methods like GetSalary, GetPromotionDetails, GetAddress, 
+//   address should be property of Person class
