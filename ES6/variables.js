90599fa13ca1e94f57cc082921bf080023c2f6f4
Ashish <ash.ishu025g@gmail.com>
2021-06-25 22:32:18 +0530

    var let const

--- ES6/variables.js
+++ ES6/variables.js
@@ -21,3 +21,45 @@
 
 // console.log(hoistVar)
 // console.log(hoistVarLet)
+
+//2.  we can redeclare and reassign values to var at any time
+
+var hoistVar = 5000;
+var hoistVar = 6000;
+hoistVar = 6000;
+
+// we cant redeclare let and but can reassign values to let at any time
+let hoistLet;
+//let hoistLet = 6000;
+hoistLet = 6000;
+
+
+// we cant redeclare let and can't reassign values to const
+// we must assign the value at the time of declaration itself
+const hoistConst = 5000;
+//hoistConst = 6000; //const is immutable 
+
+const user = {
+    name : "test",
+    age : 21
+}
+//user = {}
+
+user.name = "Adenike";
+
+
+//3. Block and Functional Scope
+
+var index = undefined;
+
+for (index = 1; index <= 5; index++) {
+    console.log(index);
+}
+
+console.log(index);
+
+for (let ind = 1; ind <= 5; ind++) {
+    console.log(ind);
+}
+
+//console.log(ind);
