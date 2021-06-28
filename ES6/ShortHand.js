8896c2fe93f278fa46c1529396ef8306c4c6019d
Ashish <ash.ishu025g@gmail.com>
2021-06-28 23:11:48 +0530

    short hand

---
+++ ES6/ShortHand.js
@@ -0,0 +1,29 @@
+// If my key value (that i am reading from any variable) are same then instead of having both of them we can have only one
+
+let cat = "Miaow",
+ dog = "Woof",
+ bird = "Chrip",
+ lion = "roar";
+
+
+ let animalSound = {
+    cat: cat,
+    dog : dog,
+    bird : bird,
+    lion : lion
+}
+
+let animalSoundShortHand = {
+    cat,
+    dog,
+    bird,
+    lion
+ }
+
+ console.log("animal sound ", animalSound) // we use comma to auto cast object
+ console.log("animal sound from ShortHand ", animalSoundShortHand) // we use comma to auto cast object
+
+ 
+ console.log("animal sound "+ animalSound) // object in incorrect format
+
+ console.log("animal sound "+ JSON.stringify(animalSound)) // to parse in to object type
