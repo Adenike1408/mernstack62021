f92d30b97982c27bd4b061a8597b7b19d6a2daa9
Ashish <ash.ishu025g@gmail.com>
2021-07-07 23:04:45 +0530

    send file

--- nodeapi/server.js
+++ nodeapi/server.js
@@ -3,15 +3,24 @@ let port = 9090;
 let express = require("express");
 let app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World Adenike!')
-})
+
 
 app.get('/test', function (req, res) {
     console.log("i am in test api")
     res.send('Hello Ashish!')
-  })
- 
+})
+
+
+app.get('/html', function (req, res) {
+  console.log("__dirname" , __dirname)
+  console.log("__filename" , __filename)
+  
+  res.sendFile(__dirname+"/index.html")
+})
+
+app.get('/', function (req, res) {
+  res.send('Hello World Adenike!')
+})
 app.listen(port);
 
 console.log(`Express server is started on port ${port}`)
