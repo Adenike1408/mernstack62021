5956bb6619d0e5dc5eb5f99f1a5563bb1b5d3607
Ashish <ash.ishu025g@gmail.com>
2021-07-08 21:30:50 +0530

    static mounting

---
+++ nodeapi/public/userdetails.js
@@ -0,0 +1,7 @@
+let user = {
+    name : "Test",
+    age : "infinte",
+    hello : "world"
+}
+
+alert(JSON.stringify(user))
--- nodeapi/server.js
+++ nodeapi/server.js
@@ -3,8 +3,12 @@ let port = 9090;
 let express = require("express");
 let app = express(); // this creates express application which will handle all request
 
+let adminApp = express(); // this app will serve the request that comes to /admin routes 
+
 //console.log("__dirname" , __dirname)
 //console.log("__filename" , __filename)
+
+app.use('/static', express.static('public')); // serve static files like images css using static middle ware
 
 app.get('/queryparam', function (req, res) {
     console.log("Query Object ", req.query)
@@ -23,9 +27,27 @@ app.get('/html', function (req, res) {
   res.sendFile(__dirname+"/index.html")
 })
 
+//path mounted to serve the request from the other concurrent express application
+app.use("/admin", adminApp);
+
+adminApp.get('/hello',(req, res)=>{
+    res.json({
+      "Application":"Admin",
+      "Msg" : "New app is mounted"
+    })
+})
+
+
 app.get('/', function (req, res) {
   res.send('Hello World Adenike!')
 })
+
+//wild card operator to match any route
+app.get('*', function (req, res) { 
+  res.send('Nothing Matched to the route you\'ve given!')
+})
+
+
 app.listen(port);
 
-console.log(`Express server is started on port ${port}`)
+console.log(`Express server is started on port ${port}`);
