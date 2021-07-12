5a1ce90a9bffec46184c983c66330a32e30bfc7f
Ashish <ash.ishu025g@gmail.com>
2021-07-12 23:22:30 +0530

    first component

---
+++ reactapp/src/app/app.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class App extends React.Component {
+
+    constructor(props){
+        super(props)
+    }
+
+
+    render(){
+        return(
+            <div>
+                <h1>The very first react component. Basically App Component</h1>
+            </div>
+        )
+    }
+
+} 
--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,1 +1,10 @@
-console.log("This is the entry point of my client side application shopping cart to be built in react")
+//console.log("This is the entry point of my client side application shopping cart to be built in react")
+import React from "react";
+import {render} from "react-dom";
+
+import App from "./app/app"
+
+render(
+    <App/>, //application that would be rendered on single page
+    document.getElementById("root") //bootstrapping the application root container
+)
