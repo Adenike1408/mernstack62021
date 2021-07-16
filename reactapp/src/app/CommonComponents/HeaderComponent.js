c6f3f576ec7360b0df243c7eee6a3316c13a7f82
Ashish <ash.ishu025g@gmail.com>
2021-07-16 20:53:18 +0530

    react start

--- reactapp/src/app/CommonComponents/HeaderComponent.js
+++ reactapp/src/app/CommonComponents/HeaderComponent.js
@@ -26,4 +26,5 @@ export function Footer(props){
     )
 };
 
+//named export 
 export const pi = 3.141;
--- reactapp/src/app/app.js
+++ reactapp/src/app/app.js
@@ -6,10 +6,31 @@ export default class App extends Component {
 
     constructor(props){
         super(props)
+
+        this.state = {
+            name : "Hung"
+        }
+
+        this.changeName();
+        this.sessionName = "MERNStack";
     }
 
+    changeName = ()=>{
+
+        setTimeout(() => {
+            console.log("this.sessionName "+this.sessionName)
+            this.sessionName = "React";
+            console.log("this.sessionName "+this.sessionName)
+            this.setState({
+                name : "Luke"
+            })
+        }, 2000);
+
+        
+    }
 
     render(){
+        console.log("Render in app ")
         let myname = "Adenike";
         let a = 5, b = 70;
         // JSX : React doesn't have any separate html templating so it creates html+js using below code
@@ -27,6 +48,8 @@ export default class App extends Component {
                 <h1>My Name is {myname}</h1>
                 <h2>Multiply {a*b}</h2>
                 <h2>Value of pi - {pi}</h2>
+                {this.sessionName}
+                <b>{this.state.name}</b>
                 <Footer />
             </div>
         )
